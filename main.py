03ed95254d8c02e0bcf777abcb48151932498bb9
Petr2508 <146702724+Petr2508@users.noreply.github.com>
2023-10-03 14:20:52 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 def print_star_digits():
     star_digits={
+        '0': ['    *****  ',
+              '  *       *', 
+              '  *       *', 
+              '  *       *', 
+              '  *       *', 
+              '  *       *', 
+              '    *****  '], 
+            
+        '1': ['        *  ', 
+              '     *  *  ', 
+              '  *     *  ', 
+              '        *  ', 
+              '        *  ', 
+              '        *  ',
+              '        *  ', 
+              '        *  '], 
         '2': ['    *  *   ', 
               '  *      * ',
               '  *       *',
@@ -34,14 +50,12 @@ def print_star_digits():
               '           *',
               '           *',
               '           *'] }
-    number=input ("Введите число (2,3 или 4): ")         
+    number=input ("Введите число (0,1,2,3 или 4): ")         
     if number in star_digits:
-            for line in star_digits[number]:
-                    print(line)
+            for line in star_digits[number1]:
+                    for i in line:
+                        temp.append(st
     else:
-            print("2,3 или 4.")
+            print("0,1,2,3 или 4.")
 print_star_digits()
               
-              
-            
-
